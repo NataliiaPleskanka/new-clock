d457add5ac80eb548ba1779006900f276679fe1d
Nataliia_Pleskanka <pleskanka.nataliia@gmail.com>
2024-05-11 16:55:43 +0200

    mm

---
+++ index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// Код у суворому режимі
+console.log("Jacob Mercer");
+console.log(26);
+const age = 20;
+console.log(age); // 20
+
+const username = "Mango";
+console.log(username); // "Mango"
